041d5850085863d9137b6915c3bc3c1470ad8d6c
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-08-04 16:10:13 +0200

    add furo and intersphinx

---
+++ doc/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import stimuli
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "simple-stimuli"
+copyright = "2022, Mathieu Scheltienne"
+author = "Mathieu Scheltienne"
+release = stimuli.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# If your documentation needs a minimal Sphinx version, state it here.
+needs_sphinx = "5.0"
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
+# ones.
+extensions = [
+    "sphinx.ext.intersphinx",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# -- Intersphinx -------------------------------------------------------------
+intersphinx_mapping = {
+    "numpy": ("https://numpy.org/devdocs", None),
+    "python": ("https://docs.python.org/3", None),
+    "matplotlib": ("https://matplotlib.org/stable", None),
+    "scipy": ("https://scipy.github.io/devdocs", None),
+    "sounddevice": ("https://python-sounddevice.readthedocs.io/", None),
+}
+intersphinx_timeout = 5
