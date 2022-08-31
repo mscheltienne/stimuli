71c340874e26b51e41936e5906a5cf88e2824018
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-08-31 17:44:40 +0200

    add sys_info

--- stimuli/__init__.py
+++ stimuli/__init__.py
@@ -1,4 +1,5 @@
 from ._version import __version__  # noqa: F401
+from .utils._config import sys_info  # noqa: F401
 from .utils._logs import (  # noqa: F401
     add_file_handler,
     add_stream_handler,
