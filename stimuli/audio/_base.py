160850fd7bc452ef5ff644ec31cc6114878b47bc
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-09-04 18:25:36 +0200

    start adding draft for Tone using BaseSound object

--- stimuli/audio/_base.py
+++ stimuli/audio/_base.py
@@ -6,12 +6,16 @@ from typing import TYPE_CHECKING
 import numpy as np
 
 from ..time import Clock
-from ..utils._checks import check_type, check_value
+from ..utils._checks import check_type, check_value, ensure_int
 from ..utils._docs import copy_doc
 from .backend import BACKENDS
 from .backend._base import BaseBackend
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from numpy.typing import NDArray
+
     from ..time import BaseClock
 
 
@@ -21,12 +25,13 @@ class BaseSound(ABC):
     @abstractmethod
     def __init__(
         self,
-        sample_rate: float,
-        duration: float,
-        device: int,
+        duration: float | NDArray[np.float32 | np.float64],
+        sample_rate: int | None,
+        device: int | None,
+        n_channels: int = 1,
         *,
-        backend: str = "sounddevice",
-        clock: BaseClock = Clock,
+        backend: str,
+        clock: BaseClock,
         **kwargs,
     ) -> None:
         check_type(backend, ("str",), "backend")
@@ -36,6 +41,12 @@ class BaseSound(ABC):
             raise ValueError(
                 "The argument 'duration' must be a strictly positive number defining "
                 f"the length of the sound in seconds. Provided '{duration}' is invalid."
+            )
+        self._n_channels = ensure_int(n_channels, "n_channels")
+        if self._n_channels < 1:
+            raise ValueError(
+                "The number of channels must be at least 1. Provided "
+                f"'{self._n_channels}' is invalid."
             )
         self._set_times()
         self._set_signal()
@@ -66,3 +77,57 @@ class BaseSound(ABC):
     @copy_doc(BaseBackend.stop)
     def stop(self) -> None:
         self._backend.stop()
+
+
+class Tone(BaseSound):
+    """Pure ton stimulus at the frequency ``f`` (Hz)."""
+
+    def __init__(
+        self,
+        frequency: float,
+        volume: float | Sequence[float],
+        duration: float,
+        sample_rate: int | None = None,
+        device: int | None = None,
+        n_channels: int = 1,
+        *,
+        backend: str = "sounddevice",
+        clock: BaseClock = Clock,
+        **kwargs,
+    ) -> None:
+        _check_frequency(frequency)
+        self._frequency = frequency
+        _check_volume(volume)
+        self._volume = volume
+        super().__init__(
+            duration,
+            sample_rate,
+            device,
+            n_channels,
+            backend=backend,
+            clock=clock,
+            **kwargs,
+        )
+
+    @copy_doc(BaseSound._set_signal)
+    def _set_signal(self) -> None:
+        tone_arr = np.sin(2 * np.pi * self._frequency * self._times)
+        tone_arr /= np.max(np.abs(tone_arr))  # normalize
+        self._signal = np.vstack((tone_arr, tone_arr)).T * self._volume / 100
+        if self._window is not None:
+            self._signal = np.multiply(self._window, self._signal.T).T
+        self._signal = self._signal.astype(np.float32)
+
+
+def _check_frequency(frequency: float) -> None:
+    """Check that the frequency is valid."""
+    check_type(frequency, ("numeric",), item_name="frequency")
+    if frequency <= 0:
+        raise ValueError(
+            f"The frequency must be a strictly positive number. Provided '{frequency}' "
+            "is invalid."
+        )
+
+
+def _check_volume(volume) -> None:
+    """Check that the volume is valid."""
