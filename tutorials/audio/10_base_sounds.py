b95fa0b8bb27fc27b980e730c309683c3e5f3cf7
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-10-01 11:47:59 +0200

    update base sound tutorial

--- tutorials/audio/10_base_sounds.py
+++ tutorials/audio/10_base_sounds.py
@@ -3,13 +3,16 @@
 Base sounds
 ===========
 
-``simple-stimuli`` provides a common API across audio stimuli. The audio
+``stimuli`` provides a common API for audio stimuli. The audio
 stimuli can be either generated or loaded. A generated stimuli can be exported.
 The volume, duration and other properties can be set when creating the stimuli
 or updated between plays.
 """
 
 # %%
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from matplotlib import pyplot as plt
 
@@ -30,7 +33,7 @@ from stimuli.audio import Sound, Tone
 # To create the stimuli, we create a :class:`~stimuli.audio.Tone` object with
 # a given volume and frequency.
 
-sound = Tone(volume=10, frequency=440)  # La - A440
+sound = Tone(frequency=440, volume=10, duration=1)  # La - A440
 
 # %%
 # We can listen to the sound we created with :meth:`~stimuli.audio.Tone.play`.
@@ -38,15 +41,10 @@ sound = Tone(volume=10, frequency=440)  # La - A440
 sound.play(blocking=True)
 
 # %%
-# We can edit the sound properties by replacing the value in the attributes.
-# For instance, let's consider a stereo system and set the volume to ``10`` on
-# the left channel and to ``30`` on the right channel.
+# We can edit the sound properties by replacing the value in the properties.
+# For instance, let's increase the volume and change the frequency.
 
-sound.volume = (10, 30)  # 0 to 100
-
-# %%
-# We can also change the frequency to 1 kHz.
-
+sound.volume = 50  # percentage between 0 and 100
 sound.frequency = 1000  # Hz
 
 # %%
@@ -55,26 +53,20 @@ sound.frequency = 1000  # Hz
 sound.play(blocking=True)
 
 # %%
-# The sampling rate can be changed. Typical values are 44.1 kHz and 48 kHz.
-
-sound.sample_rate = 48000  # Hz
-
-# %%
 # Export/Load a sound
 # -------------------
 #
-# We can export a sound with :meth:`~stimuli.audio.Tone.save`.
+# We can export a sound with :meth:`~stimuli.audio.Tone.save` and load a sound with
+# :class:`~stimuli.audio.Sound`.
 
-sound.save("my_pure_tone.wav", overwrite=True)
-
-# %%
-# We can load a sound with :class:`~stimuli.audio.Sound`.
-
-sound_loaded = Sound("my_pure_tone.wav")
+with TemporaryDirectory() as directory:
+    fname = Path(directory.name) / "my_pure_tone.wav"
+    sound.save(fname, overwrite=True)
+    sound_loaded = Sound(fname)
 sound_loaded.play(blocking=True)
 
 # %%
-# However, a loaded sound can be any type of sound. ``simple-stimuli`` does not
+# However, a loaded sound can be any type of sound. ``stimuli`` does not
 # know that the sound was exported with the ``save()`` method of one of its
 # class. As such, the attributes that were specific to the original sound are
 # not present anymore and can not be updated anymore.
@@ -93,36 +85,30 @@ print(f"Sample rate of the loaded sound: {sound_loaded.sample_rate} Hz.")
 # The volume is normalized, with the loudest channel set to ``100``. The ratio
 # between channels is preserved.
 
-print("Volume of the original sound: %s" % "({:.1f}, {:.1f})".format(*sound.volume))  # noqa
+print("Volume of the original sound: %s" % "({:.1f}, {:.1f})".format(*sound.volume))
 print(
-    "Volume of the loaded sound: %s" % "({:.1f}, {:.1f})".format(*sound_loaded.volume)  # noqa
+    "Volume of the loaded sound: %s" % "({:.1f}, {:.1f})".format(*sound_loaded.volume)
 )
 
 # %%
 # Visualize a sound
 # -----------------
 #
-# Finally, the underlying signal is stored in the ``signal`` attribute. The
-# returned numpy array has 2 dimensions: ``(n_samples, n_channels)``. We can
-# plot the signal of each channel.
+# Finally, the underlying signal is stored in the ``signal`` property, a numpy array of
+# shape ``(n_samples, n_channels)``. We can plot the signal of each channel.
 
 samples_to_plot = 100  # number of samples to plot
 times = sound.times[:samples_to_plot] * 1000  # ms
 
-f, ax = plt.subplots(2, 1, sharex=True, sharey=True)
-for k in range(2):  # 2 channels
-    # draw data
-    ax[k].plot(times, sound.signal[:samples_to_plot, k])
-    # draw horizontal line through y=0
-    ax[k].axhline(0, color="black")
+f, ax = plt.subplots(1, 1, layout="constrained")
+ax.plot(times, sound.signal.squeeze()[:samples_to_plot])  # draw data
+ax.axhline(0, color="black")  # draw horizontal line through y=0
 
 # labels
-ax[0].set_title("Right channel")
-ax[1].set_title("Left channel")
-ax[1].set_xlabel("Time (ms)")
+ax.set_title("Signal (mono)")
+ax.set_xlabel("Time (ms)")
 
 # draw vertical line after each period
 period = int(sound.sample_rate / sound.frequency)
 for k in range(0, samples_to_plot, period):
-    ax[0].axvline(times[k], color="lightgreen")
-    ax[1].axvline(times[k], color="lightgreen")
+    ax.axvline(times[k], color="lightgreen")
